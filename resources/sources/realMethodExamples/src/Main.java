407d8c64aa875e126ef2e44e1047db9f842485bc
CyrilFerlicot <cyril@ferlicot.fr>
2025-08-21 17:02:49 +0200

    Add cases of subclasses

--- resources/sources/realMethodExamples/src/Main.java
+++ resources/sources/realMethodExamples/src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         A a = new A();
         System.out.println(a.a());
+        C c = new C();
+        System.out.println(c.b());
+        D d = new D();
+        System.out.println(d.b());
     }
 }
