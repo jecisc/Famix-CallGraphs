09fcf2a873def409f499c8f5a91d2897bfb3c25f
CyrilFerlicot <cyril@ferlicot.fr>
2025-08-25 22:22:59 +0200

    Better structure for example1

--- resources/sources/example1/src/C.java
+++ resources/sources/example1/src/C.java
@@ -2,14 +2,4 @@ class C extends A {
     String m() {
         return "Hello2";
     }
-
-    public static void main(String[] args) {
-        A a = new A();
-        B b = new B();
-        String s;
-
-        s = a.m();
-        s = b.m();
-        System.out.println("Done");
-    }
 }
---
+++ resources/sources/example1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        String s;
+
+        s = a.m();
+        s = b.m();
+        System.out.println(s);
+    }
+}
