90a092bfab1c9da0ee60283725f61d086eae731f
CyrilFerlicot <cyril@ferlicot.fr>
2025-09-09 16:54:23 +0200

    e: Add second case of toString usage but this time implemented on the class

--- resources/sources/example1/src/C.java
+++ resources/sources/example1/src/C.java
@@ -1,5 +1,12 @@
 class C extends A {
+
+    @Override
     public String m() {
+        return this.toString() + "2";
+    }
+
+    @Override
+    public String toString() {
         return "Hello2";
     }
 }
