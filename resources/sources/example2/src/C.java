509fff630cf83cfecb254fafb24262d2227e8b28
CyrilFerlicot <cyril@ferlicot.fr>
2025-09-12 17:31:02 +0200

    e: Add new resource file

---
+++ resources/sources/example2/src/C.java
@@ -0,0 +1,11 @@
+public class C implements I2{
+    @Override
+    public String method1() {
+        return "C";
+    }
+
+    @Override
+    public String method5() {
+        return "C2";
+    }
+}
