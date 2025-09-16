01467a747706ad28dd5228c02ce42e26a0958ce5
CyrilFerlicot <cyril@ferlicot.fr>
2025-09-16 16:32:36 +0200

    e: Add two methods with same signature

---
+++ resources/sources/example3/src/A.java
@@ -0,0 +1,9 @@
+public class A {
+    public String m(){
+        return "hello";
+    }
+
+    public String m(String m){
+        return m;
+    }
+}
---
+++ resources/sources/example3/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        A a =  new A();
+        System.out.println(a.m());
+        System.out.println(a.m("a"));
+    }
+}
