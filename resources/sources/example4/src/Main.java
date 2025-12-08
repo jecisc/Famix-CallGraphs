bbf0ec19c62c89f95c14d8fcd97c421f01abe255
CyrilFerlicot <cyril@ferlicot.fr>
2025-12-08 19:17:12 +0100

    Simplify tests

--- resources/sources/example4/src/Main.java
+++ resources/sources/example4/src/Main.java
@@ -6,27 +6,16 @@ import typeParameter.SimpleObject;
 public class Main {
     public static void main(String[] args) {
         Box<Integer> intBox = new Box<>(42);
-        System.out.println(intBox);
         intBox.set(100);
         System.out.println(intBox.get());
 
-        Box<String> strBox = new Box<>("hello");
-        System.out.println(strBox);
-
-        Integer[] numbers = {1, 2, 3, 4};
-        String[] words   = {"alpha", "beta", "gamma"};
-
-        Integer firstNumber = Util.first(numbers);
-        String  firstWord   = Util.first(words);
-
-        System.out.println("First number: " + firstNumber);
-        System.out.println("First word: "   + firstWord);
+        System.out.println("First number: " + Util.first(new Integer[]{1, 2, 3, 4}));
 
         Box<? extends Number> numBox = new Box<>(3.14);
         System.out.println(numBox);
 
-        ParametricCollectionUser user = new ParametricCollectionUser();
-        user.getCollection().method3(new SimpleObject());
-        System.out.println(user.getCollection());
+        //ParametricCollectionUser user = new ParametricCollectionUser();
+        //user.getCollection().method3(new SimpleObject());
+        //System.out.println(user.getCollection());
     }
 }
